4605cf634e53e16b523fa024b1673d825a7b96de
Jafar Madadi <jafar.madadi2026@gmail.com>
2026-04-28 13:14:15 +0430

    feat(admin-routes): implement admin authentication routes and dashboard access control

--- routes/admin.php
+++ routes/admin.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->prefix('admin')->as('admin.')->group(function () {
+Route::middleware('guest:admin')->prefix('admin')->as('admin.')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
